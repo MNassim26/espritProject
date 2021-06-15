91faddbfe714e42665679a805a274b7d09fa3316
MNassim26 <nassim.mekki8@gmail.com>
2021-06-15 02:24:32 +0100

    categories and products Data Injection & fixed products delete bug

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -5,11 +5,14 @@ namespace App\Controller;
 use App\Entity\Category;
 use App\Entity\Product;
 use App\Form\CategoryFormType;
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 class CategoryController extends AbstractController
 {
@@ -88,4 +91,48 @@ class CategoryController extends AbstractController
         return $this->render('category/update.html.twig', array("form" => $form->createView()));
 
     }
+
+    /**
+    * @Route("/uploadCategories", name="uploadCategories")
+    */
+    public function uploadCategories(Request $request)
+    {
+    $file = $request->files->get('myfile'); // get the file from the sent request
+    $fileFolder = __DIR__ . '/../../public/upload/';  //choose the folder in which the uploaded file will be stored
+    $fileDate= new DateTime();
+    $fileDate = $fileDate->format('d-m-Y');
+    $filePathName = $fileDate."-". $file->getClientOriginalName();
+   // apply md5 function to generate an unique identifier for the file and concat it with the file extension  
+         try {
+             $file->move($fileFolder, $filePathName);
+         } catch (FileException $e) {
+             dd($e);
+         }
+    $spreadsheet = IOFactory::load($fileFolder . $filePathName); 
+    $row = $spreadsheet->getActiveSheet()->removeRow(1); // I added this to be able to remove the first file line 
+    $sheetData = $spreadsheet->getActiveSheet()->toArray(null, true, true, true); // here, the read data is turned into an array
+    $em = $this->getDoctrine()->getManager(); 
+    $succes=0;
+    foreach ($sheetData as $Row) 
+        { 
+
+            $name = $Row['A']; 
+
+            $category_existant = $em->getRepository(Category::class)->findOneBy(array('name' => $name)); 
+                // make sure that the user does not already exists in your db 
+            if (!$category_existant) 
+             {  
+                $category = new Category(); 
+                $category->setName($name);          
+                $em->persist($category); 
+                $em->flush(); 
+                 // here Doctrine checks all the fields of all fetched data and make a transaction to the database.
+                 if($succes ==0){
+                    $this->addFlash('categoryDataUploaded', 'The category data from the excel file has been added');
+                    $succes=1;
+                 }
+             } 
+        }  
+        return $this->redirectToRoute("listCategories"); 
+        }      
 }
--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,8 +3,10 @@
 
 namespace App\Controller;
 
-
+use App\Entity\Category;
+use App\Entity\Order;
 use App\Entity\Product;
+use App\Entity\Supplier;
 use App\Form\ProductFormType;
 use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -14,6 +16,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 
 class ProductController extends AbstractController
@@ -68,17 +72,20 @@ class ProductController extends AbstractController
     public function deleteProduct($id)
     {
         $product = $this->getDoctrine()->getRepository(Product::class)->find($id);
-        if($product->getOrders() !=null){
-                $this->addFlash('productDeleteError', 'This product can not be deleted, it is already ordered !');
-                return $this->redirectToRoute("listProducts");
-        } 
-        else{
-            $em = $this->getDoctrine()->getManager();
-            $em->remove($product);
-            $em->flush();
-            $this->addFlash('productDeleted', 'The product has been deleted');
-            return $this->redirectToRoute("listProducts");
+        $orders= $this->getDoctrine()->getRepository(Order::class)->findAll();
+        foreach($orders as $order){
+            foreach($order->getProducts() as $oProduct){
+                if($oProduct == $product){
+                    $this->addFlash('productDeleteError', 'This product can not be deleted, it is already ordered !');
+                    return $this->redirectToRoute("listProducts");
+                }
+            }
         }
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($product);
+        $em->flush();
+        $this->addFlash('productDeleted', 'The product has been deleted');
+        return $this->redirectToRoute("listProducts");
     }
 
     /**
@@ -157,6 +164,81 @@ class ProductController extends AbstractController
         $this->addFlash('ExcelFileSaved', 'Excel file saved');
         return $this->redirectToRoute('listProducts');
     }
+
+    /**
+    * @Route("/uploadProducts", name="uploadProducts")
+    */
+    public function uploadProducts(Request $request)
+    {
+    $file = $request->files->get('myfile'); // get the file from the sent request
+    $fileFolder = __DIR__ . '/../../public/upload/';  //choose the folder in which the uploaded file will be stored
+    $fileDate= new DateTime();
+    $fileDate = $fileDate->format('d-m-Y');
+    $filePathName = $fileDate."-". $file->getClientOriginalName();
+   // apply md5 function to generate an unique identifier for the file and concat it with the file extension  
+         try {
+             $file->move($fileFolder, $filePathName);
+         } catch (FileException $e) {
+             dd($e);
+         }
+    $spreadsheet = IOFactory::load($fileFolder . $filePathName); 
+    $row = $spreadsheet->getActiveSheet()->removeRow(1); // I added this to be able to remove the first file line 
+    $sheetData = $spreadsheet->getActiveSheet()->toArray(null, true, true, true); // here, the read data is turned into an array
+    $em = $this->getDoctrine()->getManager(); 
+    $succes=0;
+    foreach ($sheetData as $Row) 
+        { 
+
+            $name = $Row['A'];
+            $price = $Row['B']; 
+            $quantity = $Row['C'];
+            $categoryName = $Row['D'];
+            $supplierName = $Row['E'];    
+
+            $product_existant = $em->getRepository(Product::class)->findOneBy(array('name' => $name)); 
+                // make sure that the user does not already exists in your db 
+            if (!$product_existant) 
+             {  
+                $product = new Product(); 
+                $product->setName($name);
+                $product->setPrice($price); 
+                $product->setQuantity($quantity);
+                $category_existant = $em->getRepository(Category::class)->findOneBy(array('name' => $categoryName));
+                if (!$category_existant){
+                    $category = new Category();
+                    $category->setName($categoryName);
+                    $em = $this->getDoctrine()->getManager();
+                    $em->persist($category);
+                    $em->flush();
+                    $product->setCategory($category);
+                } 
+                else {
+                    $product->setCategory($category_existant);
+                }
+                $supplier_existant = $em->getRepository(Category::class)->findOneBy(array('name' => $supplierName));
+                if (!$supplier_existant){
+                    $supplier = new Supplier();
+                    $supplier->setName($supplierName);
+                    $em = $this->getDoctrine()->getManager();
+                    $em->persist($supplier);
+                    $em->flush();
+                    $product->setSupplier($supplier);
+                } 
+                else{
+                    $product->setSupplier($supplier_existant);
+                }
+                $product->setOrders(null);
+                $em->persist($product); 
+                $em->flush(); 
+                 // here Doctrine checks all the fields of all fetched data and make a transaction to the database.
+                 if($succes ==0){
+                    $this->addFlash('productDataUploaded', 'The product data from the excel file has been added');
+                    $succes=1;
+                 }
+             } 
+        }  
+        return $this->redirectToRoute("listProducts"); 
+        }     
 
 
 }

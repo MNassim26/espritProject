28b95f470c22860bd69975fa7cb50cb89e386dca
MNassim26 <nassim.mekki8@gmail.com>
2021-06-18 02:38:02 +0100

    custom ProductFormType

--- src/Form/ProductFormType.php
+++ src/Form/ProductFormType.php
@@ -2,8 +2,16 @@
 
 namespace App\Form;
 
+use App\Entity\Category;
 use App\Entity\Product;
+use App\Entity\Supplier;
+use App\Repository\CategoryRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,11 +20,39 @@ class ProductFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('price')
-            ->add('quantity')
-            ->add('category')
-            ->add('supplier')
+            ->add('name', TextType::class , [
+                'attr'=> [
+                    'class'=>'form-control'
+                    ]
+                ])
+            ->add('price', TextType::class , [
+                'attr'=> [
+                    'class'=>'form-control'
+                    ]
+                ])
+            ->add('quantity', NumberType::class , [
+                'attr'=> [
+                    'class'=>'form-control'
+                    ]
+                ])
+            ->add('category',EntityType::class , [
+                'attr'=> [
+                    'class'=>'form-control'
+                ],
+                'required' => false,
+                'placeholder' => 'Veuillez choisir une catégorie',
+                'class' => Category::class,
+                'choice_label' => 'name',
+            ])
+            ->add('supplier',EntityType::class , [
+                'attr'=> [
+                    'class'=>'form-control'
+                ],
+                'required' => false,
+                'placeholder' => 'Veuillez choisir un fournisseur',
+                'class' => Supplier::class,
+                'choice_label' => 'name',
+            ])
             // ->add('orders')
         ;
     }

1f6d21aba797629aa9a6496f8216acfb31673863
MNassim26 <nassim.mekki8@gmail.com>
2021-06-19 02:10:50 +0100

    Supplier add and update pages

--- src/Form/SupplierFormType.php
+++ src/Form/SupplierFormType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Supplier;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +13,16 @@ class SupplierFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('adress')
+            ->add('name', TextType::class , [
+                'attr'=> [
+                    'class'=>'form-control'
+                    ]
+                ])
+            ->add('adress', TextType::class , [
+                'attr'=> [
+                    'class'=>'form-control'
+                    ]
+                ])
         ;
     }
 
